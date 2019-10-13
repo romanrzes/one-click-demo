510690cde5783da9c89b93e3a4a40071e5ac1cb2
Ubuntu <ubuntu@ip-10-0-0-238.eu-central-1.compute.internal>
2019-10-13 12:19:35 +0000

    Added elb_db

--- private.tf
+++ private.tf
@@ -58,3 +58,47 @@ resource "aws_security_group" "db_instance" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+output "elb_db_dns_name" {
+  value = "${aws_elb.elb_db.dns_name}"
+}
+
+resource "aws_elb" "elb_db" {
+  name = "elb_db"
+  subnets = ["${aws_subnet.eu-central-1-private.id}"]
+
+  listener {
+    lb_port           = "${var.http_port}"
+    lb_protocol       = "${var.protocol_http}"
+    instance_port     = "${var.http_port}"
+    instance_protocol = "${var.protocol_http}"
+  }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    interval            = 30
+    target              = "HTTP:${var.http_port}/"
+  }
+  instances = ["${aws_instance.db_master.id}", "${aws_instance.db_slave.id}"] 
+}
+
+resource "aws_security_group" "elb_db_sec" {
+  name = "elb_db_sec"
+
+  ingress {
+    from_port   = "${var.http_port}"
+    to_port     = "${var.http_port}"
+    protocol    = "${var.protocol_tcp}"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
